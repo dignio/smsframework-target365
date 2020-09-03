0bbb5242ca3e1298bb2ae3c96cfa2860fbc07090
Ilya Vihtinsky <iliaviht@gmail.com>
2020-09-03 16:58:30 +0300

    Changed version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     # http://pythonhosted.org/setuptools/setuptools.html
     name='smsframework-target365',
-    version='0.0.1-0',
+    version='0.0.2-0',
     author='Ilya Vihtinsky',
     author_email='iliaviht@gmail.com',
 
